483cb15b5db7cf27d722742c8a7ea8c04f563c40
Thorsten M <thoto@devtal.de>
2024-05-01 22:29:29 +0000

    less tapping duration, left hand cursor keys, symbols on homerow

--- keyboards/handwired/dactyl_manuform/4x5_thoto/keymaps/thoto/config.h
+++ keyboards/handwired/dactyl_manuform/4x5_thoto/keymaps/thoto/config.h
@@ -24,7 +24,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define MASTER_LEFT
 //#define MASTER_RIGHT
 
-#define TAPPING_TERM 350
+#define TAPPING_TERM 250
 #define HOLD_ON_OTHER_KEY_PRESS
 // #define PERMISSIVE_HOLD
 
